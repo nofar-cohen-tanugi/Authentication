03b541e313211ec5d1cd99d405d473269a3d4118
= <nofarcohen19@gmail.com>
2023-10-10 20:27:31 +0300

    ui login and post request

---
+++ Frontend/src/component/login/loginFormSettings.tsx
@@ -0,0 +1,24 @@
+import { UseControllerProps } from 'react-hook-form';
+import { ILoginForm } from '../../models/login/ILoginForm.model';
+
+export const loginFormSettings: Record<
+  keyof ILoginForm,
+  UseControllerProps<ILoginForm, keyof ILoginForm>
+> = {
+  email: {
+    name: 'email',
+    rules: {
+      required: { value: true, message: 'required field' },
+      pattern: {
+        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        message: 'Invalid email address',
+      },
+    },
+  },
+  password: {
+    name: 'password',
+    rules: {
+      required: { value: true, message: 'required field' },
+    },
+  },
+};
