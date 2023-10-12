f26733b0e36c4ec6ad607b82c64402a72c45781a
= <nofarcohen19@gmail.com>
2023-10-12 13:02:55 +0300

    add table

---
+++ Frontend/src/redux/project/projectApi.ts
@@ -0,0 +1,30 @@
+// Or from '@reduxjs/toolkit/query' if not using the auto-generated hooks
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { ProjectDto } from '../../models/project/projectDto.model'
+import { RootState } from '../store';
+
+export const projectApi = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/',
+    prepareHeaders: (headers, { getState }) => {
+      // By default, if we have a token in the store, let's use that for authenticated requests
+      const token = (getState() as RootState).authSlice.token;
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`)
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ['Project'],
+  endpoints: (build) => ({
+    // The query accepts a number and returns a Post
+    getProjects: build.query<ProjectDto[], void>({
+      query: () => ({
+        url: `https://private-052d6-testapi4528.apiary-mock.com/info`,
+        method: 'GET',
+      })
+    }),
+  }),
+})
+
+export const { useGetProjectsQuery } = projectApi
